649e6db34f16d93aca72fbdecec8b52e122fb949
Fatan Izzatur <izzaturziyech22@gmail.com>
2024-06-18 18:24:54 +0700

    mengubah sample object

--- src/library/LibraryManagement.java
+++ src/library/LibraryManagement.java
@@ -5,9 +5,9 @@ import javax.swing.SwingUtilities;
 public class LibraryManagement {
     public static void main(String[] args) {
         // Create sample objects
-        Book book1 = new Book("The Great Gatsby", "B001", "F. Scott Fitzgerald", 180);
-        Member member1 = new Member("Alice Johnson", "M001");
-        Librarian librarian1 = new Librarian("Bob Smith", "L001");
+        Book book1 = new Book("Laskar Pelangi", "B001", "Andrea Hirata", 180);
+        Member member1 = new Member("Ahmad Fikri", "M001");
+        Librarian librarian1 = new Librarian("Agus Setiawan", "L001");
 
         // Display information
         book1.displayInfo();
